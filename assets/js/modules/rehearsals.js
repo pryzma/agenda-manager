18ce20ce2599f6f6e063834349ace681e0d8cec3
Fides <fides.torres@outlook.com>
2019-05-16 11:26:22 +0200

    rehearsal

---
+++ assets/js/modules/rehearsals.js
@@ -0,0 +1,11 @@
+/*
+* assets/js/modules/rehearsals.js
+*/
+'use strict'
+const rehearsals = (function(){
+  const main = function() {}
+  return {
+    name : 'Rehearsals',
+    default : main
+  }
+})();
