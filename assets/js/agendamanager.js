46da6356789ac3eea4997a8c1e5bec48ee8ca86d
Christoffel Colombijn <ccolombijn@gmail.com>
2019-05-17 14:24:14 +0200

    Update agendamanager.js

--- assets/js/agendamanager.js
+++ assets/js/agendamanager.js
@@ -15,28 +15,36 @@ const agendamanager = (function() {
   // get template for current module
   const getTemplate = (route) => applicationObj()[route].template ? applicationObj()[route].template : 'pageLayout'
 
-  const initPage = function(route) {
+  const initPage = function(route,callback) {
+    if(typeof route === 'function' ) {
+      callback = route;
+      route = applicationRoute()[0]
+    }
     if(!route) route = applicationRoute()[0]
+
     // set current nav item as active
     updateNav( route );
     // set page title
     setPageTitle( route );
     // load page
-    loadPage( route );
+    loadPage( route, callback );
+
   }
 
   // set default nav item as active
   $( `${config.nav} li#${config.default} a` ).addClass('active');
-
+  // set selected nav item as active
   const updateNav = function(route) {
 
     $(`${config.nav} a`).removeClass('active');
     ( route === '') ?
     $(`${config.nav} li#${config.default} a`).addClass('active')
     : $(`${config.nav} li#${applicationRoute()[0]} a`).addClass('active');
+
   }
   // set page title with application and module name
   const setPageTitle = function( route ) {
+    
     if(route === '') route = config.default;
     let pageTitle = ( route === '') ?
     applicationObj().name
@@ -45,31 +53,32 @@ const agendamanager = (function() {
     $('title').html(pageTitle);
   }
   // load template html file
-  const loadPage = function(route) {
+  const loadPage = function( route, callback ) {
+
     if(route === '') route = config.default;
+    console.log(`loadPage : ${route}`)
     let template = getTemplate(route);
     $(config.main).load(`html/templates/${template}.html`,function() {
       $(`#${template} h2`).html(getModuleName(route));
+      if(callback) callback();
     });
-  }
 
+  }
 
   // init is loaded once when the application is loaded
   const init = function() {
     for(let module of Object.getOwnPropertyNames(applicationObj())){
       // attach initPage as callbefore for each module
-      if(applicationObj()[module].name) agendamanager[ module ][ 'callbefore' ] = initPage
+      //if(applicationObj()[module].name) agendamanager[ module ][ 'callbefore' ] = initPage
     }
-
   }
-  // callbefore is loaded before each time a module is loaded
-  const callbefore = () => initPage()
+
 
   return {
     config : config,
     name : 'Agenda Manager',
     init : init,
-    callbefore : callbefore,
+    initPage : initPage,
     // modules
     dashboard : dashboard,
     shows : shows,
