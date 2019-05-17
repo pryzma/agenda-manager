4822b379cf6b9df1ec8d7a2e11bae8fc55952b05
Christoffel Colombijn <ccolombijn@gmail.com>
2019-05-17 11:58:55 +0200

    Fixed initPage

--- assets/js/agendamanager.js
+++ assets/js/agendamanager.js
@@ -8,58 +8,68 @@ const agendamanager = (function() {
     nav : 'nav#headerNav',
     default : 'dashboard'
   }
-  // callback is loaded after application & module is loaded
-  const callback = function() {
-    const applicationObj = application.object(); // application & modules object
-    const applicationRoute = application.route(); // current route
-    const getModuleName = (route) => applicationObj[route].name
-    const getTemplate = (route) => applicationObj[route].template ? applicationObj[route].template : 'pageLayout'
-    const loadPage = function(route) {
-      let template = getTemplate(route);
-      $(config.main).load(`html/templates/${template}.html`,function() {
-        $(`#${template} h2`).html(getModuleName(route));
-      });
-    }
-    const initPage = function(route) {
-      // set current nav item as active on hashchange
-      updateNav( route );
 
-      // set page title
-      setPageTitle( route );
+  const applicationObj = () => application.object(); // application & modules object
+  const applicationRoute = () => application.route(); // current route
+  const getModuleName = (route) => applicationObj()[route].name // current module name
+  // get template for current module
+  const getTemplate = (route) => applicationObj()[route].template ? applicationObj()[route].template : 'pageLayout'
 
-      // load page
-      loadPage( route );
-    }
+  const initPage = function(route) {
+    if(!route) route = applicationRoute()[0]
+    // set current nav item as active
+    updateNav( route );
+    // set page title
+    setPageTitle( route );
+    // load page
+    loadPage( route );
+  }
 
+  // set default nav item as active
+  $( `${config.nav} li#${config.default} a` ).addClass('active');
 
+  const updateNav = function(route) {
 
-    const updateNav = function(route) {
-      $(`${config.nav} a`).removeClass('active');
-      ( route === '') ?
-      $(`${config.nav} li#${config.default} a`).addClass('active')
-      : $(`${config.nav} li#${applicationRoute[0]} a`).addClass('active');
-    }
-    const setPageTitle = function( route ) {
-      let pageTitle = ( route === '') ?
-      `${applicationObj.name}`
-      : `${applicationObj.name} ${getModuleName(route)}`;
+    $(`${config.nav} a`).removeClass('active');
+    ( route === '') ?
+    $(`${config.nav} li#${config.default} a`).addClass('active')
+    : $(`${config.nav} li#${applicationRoute()[0]} a`).addClass('active');
+  }
+  // set page title with application and module name
+  const setPageTitle = function( route ) {
+    if(route === '') route = config.default;
+    let pageTitle = ( route === '') ?
+    applicationObj().name
+    : `${getModuleName(route)} - ${applicationObj().name}`;
 
-      $('title').html(pageTitle);
-    }
-    // set default nav item as active
-    $( `${config.nav} li#${config.default} a` ).addClass('active');
-    
-    initPage( applicationRoute[0] );
-    $( window ).on('hashchange',function(event) {
-      initPage(applicationRoute[0]);
+    $('title').html(pageTitle);
+  }
+  // load template html file
+  const loadPage = function(route) {
+    if(route === '') route = config.default;
+    let template = getTemplate(route);
+    $(config.main).load(`html/templates/${template}.html`,function() {
+      $(`#${template} h2`).html(getModuleName(route));
     });
+  }
+
+
+  // init is loaded once when the application is loaded
+  const init = function() {
+    for(let module of Object.getOwnPropertyNames(applicationObj())){
+      // attach initPage as callbefore for each module
+      if(applicationObj()[module].name) agendamanager[ module ][ 'callbefore' ] = initPage
+    }
 
   }
+  // callbefore is loaded before each time a module is loaded
+  const callbefore = () => initPage()
 
   return {
     config : config,
     name : 'Agenda Manager',
-    callback : callback,
+    init : init,
+    callbefore : callbefore,
     // modules
     dashboard : dashboard,
     shows : shows,
