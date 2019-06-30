d2fe9086f11a873f5ce88f9322d8c9c94e150d08
nstolk <nick_stolk9@hotmail.com>
2019-06-30 14:31:58 +0200

    Setup .env and add dotenv module

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const dotenv = require('dotenv').config();
 const express = require( 'express' );
 const app = express();
 app.use(express.static( 'assets' ));
-const server = app.listen(3000, () => {
-  console.log( 'Listening to port 3000' );
+
+const server = app.listen(process.env.PORT, () => {
+  console.log( `Listening to port ${process.env.PORT}` );
 });
