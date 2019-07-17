9f063186c0881be3189e999ba2f513350ac57310
Christoffel Colombijn <ccolombijn@gmail.com>
2019-07-17 07:43:55 +0200

    Update agendamanager.js

--- agendamanager.js
+++ agendamanager.js
@@ -14,11 +14,28 @@ const agendamanager = (function(){
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, 'assets')));
+  const index = require('./routes/index');
+  app.use('/', index);
   // set routes
   for(let _route of Object.getOwnPropertyNames(config.routes)){
     let _module = require(`./routes/${_route}`);
     app.use(_route, _module);
   }
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 })();
 module.exports = agendamanager;
