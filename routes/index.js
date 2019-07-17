635d4f0072e88530b47d98e2847264e2e74123d5
Christoffel Colombijn <ccolombijn@gmail.com>
2019-07-17 08:59:39 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = require('../router')
+router('index',(req,res,next)=>{
+  profileName : res.session.user.profileName
 });
-
 module.exports = router;
