b736ad9f388aac3d7d1a0b164d51cf2d83fc22cb
Christoffel Colombijn <ccolombijn@gmail.com>
2019-07-18 07:13:36 +0200

    Update router.js

--- router.js
+++ router.js
@@ -4,7 +4,12 @@ const router = function(route,_render){
   const express_router = express.Router();
   const _route = route === 'index' ? '/' : route
   router.get(route, isAuthenticated, function(req, res, next) {
-    res.render(route, _render(req, res, next));
+    if(typeof _render(req, res, next) === 'object'){
+      res.render(route, _render(req, res, next));
+    }else{
+      _render(req, res, next);
+    }
+      
   });
   function isAuthenticated(req, res, next) {
     if (req.session.user)
