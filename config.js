5ffe49b211f71e6ec5fea32e4c6d54e66fcd19fd
Christoffel <ccolombijn@gmail.com>
2019-07-30 20:33:24 +0200

    Updated config & repo cleanup

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-module.exports = function(package,app){
-  const config = require('./assets/json/config.json');
-    app.get('/config', (_req, res) => {
-      
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(_config));
-    });
-    config.version = package.version;
-    return config;
-  }
+const _config = require('./assets/json/config.json');
+function config(package,app){
+  app.get('/config', (_req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(_config));
+  });
+   _config.version = package.version;
+  return _config;
+}
+module.exports = config;
